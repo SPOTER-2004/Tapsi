1d63e5079515b2e13e44c266fa1272235d9fa506
SPOTER-2004 <stady2022@gmail.com>
2024-08-12 15:47:19 +0330

    add Tapsi Service box

--- src/script.js
+++ src/script.js
@@ -1,31 +1,32 @@
 function menu() {
-    document.getElementById("DropDownMenu").classList.toggle("show");
-  }
+  document.getElementById("DropDownMenu").classList.toggle("show");
+}
 
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      for (var i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
     }
   }
+};
 
+const contentBoxes = document.querySelectorAll(".content-box");
+const contents = document.querySelectorAll(".content");
+const content_1 = document.getElementById("content1");
 
-
-  const contentBoxes = document.querySelectorAll('.content-box');
-  const contents = document.querySelectorAll('.content');
-
-  contentBoxes.forEach((box, index) => {
-      box.addEventListener('click', () => {
-          contents.forEach(content => content.classList.remove('active'));
-          contentBoxes.forEach(b => b.classList.remove('active'));
-          
-          const contentToShow = document.getElementById(`content${index + 1}`);
-          contentToShow.classList.add('active');
-          box.classList.add('active');
-      });
+contentBoxes.forEach((box, index) => {
+  box.addEventListener("click", () => {
+    contents.forEach((content) => content.classList.remove("flex"));
+    contentBoxes.forEach((b) => {
+      b.classList.remove("active");
+    });
+    
+    const contentToShow = document.getElementById(`content${index + 1}`);
+    contentToShow.classList.add("flex");
+    box.classList.add("active");
   });
+});
